ad9f02370c1359e203541f36ba7c13b78592e880
Jose <impacevanend@gmail.com>
2025-11-05 10:16:52 -0500

    se modifica prueba

--- jest.config.js
+++ jest.config.js
@@ -2,21 +2,18 @@ export default {
   testEnvironment: 'node',
   verbose: true,
 
-  // dónde están los tests
+  
   roots: ['<rootDir>/tests'],
 
-  // no usamos transform (todo es ESM nativo)
+  
   transform: {},
 
-  // extensiones que Jest debe resolver
+ 
   moduleFileExtensions: ['js', 'json'],
 
-  // cobertura (opcional)
   collectCoverageFrom: ['src/**/*.js', '!src/server.js'],
 
-  // cargar variables de entorno de .env
   setupFiles: ['dotenv/config'],
 
-  // desconectar Prisma al terminar toda la corrida
   globalTeardown: '<rootDir>/tests/utils/teardown.js',
 };
